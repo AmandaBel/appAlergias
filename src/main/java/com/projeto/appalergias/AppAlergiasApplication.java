481d29dd30362b343908907479dadde3b6f31f77
Amanda Beltoso <amandabeltoso@gmail.com>
2021-04-09 10:50:50 -0300

    projeto criado

---
+++ src/main/java/com/projeto/appalergias/AppAlergiasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.appalergias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAlergiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAlergiasApplication.class, args);
+	}
+
+}
